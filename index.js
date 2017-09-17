34e35e676d6548a238a097838df87090de9b6c11
Vladimir de Turckheim <vlad2t@hotmail.com>
2017-09-17 20:06:56 +0200

    tests ready

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const MAX_N = 46; // after 46, the max limit of int ie reached
-const NB_RUN = 1000;
+const NB_RUN = 10000;
 
 const METHOD = 'loop';
 
@@ -15,8 +15,8 @@ for (let target of targets){
     res[target] = new Array(MAX_N);
     for (let i = 1; i < MAX_N; ++i) {
 
-        process.stdout.write('node ./runner ' + METHOD + target + ' ' + i + ' ' + NB_RUN);
-        process.stdout.write('...');
+        process.stdout.write('node ./runner ' + METHOD + ' ' + target + ' ' + i + ' ' + NB_RUN);
+        process.stdout.write(' ... ');
         const result = CP.execSync('node ./runner ' + METHOD + ' ' + target + ' ' + i + ' ' + NB_RUN).toString();
         process.stdout.write(result);
         res[target][i] = parseFloat(result);
